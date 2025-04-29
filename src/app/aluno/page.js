5522333c7e0dc3d54d02c37ce3c10161461ef0a9
Venícios Vasconcelos <venicios.vasconcelos@academico.ifpb.edu.br>
2025-04-29 09:59:52 -0300

    modificações no dbeaver

--- src/app/aluno/page.js
+++ src/app/aluno/page.js
@@ -1,6 +1,18 @@
-export default () => (
-       <>
-       <h1>Vamos comer</h1>
-       </>
-
-)
+import db from "@/lib/db"
+export default async () => {
+    const alunos = await db.query("select * from usuario")
+ return (<>
+    <h1>Lista de alunos</h1>
+    <div>
+      {
+         alunos.rows.map( 
+            a => (
+               <div>
+                  {a.nome} trabalha sendo {a.cargo}
+               </div>
+            ) 
+         )
+      }
+   </div>
+ </>);
+}
